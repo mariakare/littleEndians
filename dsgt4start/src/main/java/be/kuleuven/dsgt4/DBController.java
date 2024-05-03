3a13e92b31e1a8ff2e65a6ecb6da9012d0aa5719
bmas <benedictamarietta.amanda@student.kuleuven.be>
2024-05-03 12:26:52 +0200

    moved getBundles and newUser to DBController

---
+++ dsgt4start/src/main/java/be/kuleuven/dsgt4/DBController.java
@@ -0,0 +1,105 @@
+package be.kuleuven.dsgt4;
+
+import be.kuleuven.dsgt4.auth.WebSecurityConfig;
+import com.google.cloud.firestore.Firestore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.access.AuthorizationServiceException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import static java.util.stream.Collectors.*;
+import java.util.*;
+
+@RestController
+class DBController {
+
+    @Autowired
+    Firestore db;
+
+    @PostMapping("/api/newUser")
+    @ResponseBody
+    public User newuser() throws InterruptedException, ExecutionException {
+        var user = WebSecurityConfig.getUser();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("user", user.getEmail().toString());
+        data.put("role", "manager");
+
+        this.db.collection("user").document(user.getEmail().toString()).set(data);
+
+        return user;
+    }
+
+
+
+    @GetMapping("/api/getBundles")
+    public String getBundles() throws InterruptedException, ExecutionException {
+        //required level: user
+        var user = WebSecurityConfig.getUser();
+
+        /*
+         * Everything in here is absolute bs
+         * I am just using this as a temp to test a dynamic page
+         */
+
+
+        String jsonData = "{\n" +
+                "  \"bundles\": [\n" +
+                "    {\n" +
+                "      \"name\": \"Bundle 1\",\n" +
+                "      \"description\": \"Bundle 1 description goes here.\",\n" +
+                "      \"products\": [\n" +
+                "        {\n" +
+                "          \"name\": \"Product 1\",\n" +
+                "          \"description\": \"Short description for Product 1. Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes\",\n" +
+                "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"name\": \"Product 2\",\n" +
+                "          \"description\": \"Short description for Product 2.\",\n" +
+                "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"name\": \"Product 3\",\n" +
+                "          \"description\": \"Short description for Product 3.\",\n" +
+                "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"Bundle 2\",\n" +
+                "      \"description\": \"Bundle 2 description goes here.\",\n" +
+                "      \"products\": [\n" +
+                "        {\n" +
+                "          \"name\": \"Product 4\",\n" +
+                "          \"description\": \"Short description for Product 4.\",\n" +
+                "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"name\": \"Product 5\",\n" +
+                "          \"description\": \"Short description for Product 5.\",\n" +
+                "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"name\": \"Product 6\",\n" +
+                "          \"description\": \"Short description for Product 6.\",\n" +
+                "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        return jsonData;
+    }
+
+}
