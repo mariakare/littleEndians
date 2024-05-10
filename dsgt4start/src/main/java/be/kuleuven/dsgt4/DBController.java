a66c3e6ca77d7138c56cff8f486a279d1377b5c5
mariakare <mariakaregla4@gmail.com>
2024-05-10 15:00:28 +0200

    before merge with main

--- dsgt4start/src/main/java/be/kuleuven/dsgt4/DBController.java
+++ dsgt4start/src/main/java/be/kuleuven/dsgt4/DBController.java
@@ -1,6 +1,8 @@
 package be.kuleuven.dsgt4;
 
 import be.kuleuven.dsgt4.auth.WebSecurityConfig;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.firestore.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,13 +12,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.google.api.core.ApiFuture;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 import java.util.*;
 
 @RestController
 class DBController {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @Autowired
     Firestore db;
@@ -37,15 +44,152 @@ class DBController {
 
 
 
+    @PostMapping("/api/addToCart")
+    public ResponseEntity<String> addToCart(@RequestBody String bundleId) throws ExecutionException, InterruptedException {
+        // Get the current user's ID
+        var user = WebSecurityConfig.getUser();
+
+        // Reference to the user's document
+        DocumentReference userRef = db.collection("user").document(user.getEmail());
+
+        // Reference to the bundle document
+        DocumentReference bundleRef = db.collection("bundles").document(bundleId);
+
+        // Get the bundle data
+        ApiFuture<DocumentSnapshot> bundleFuture = bundleRef.get();
+        DocumentSnapshot bundleSnapshot = bundleFuture.get();
+        if (bundleSnapshot.exists()) {
+            Map<String, Object> bundleData = bundleSnapshot.getData();
+
+            // Add the bundle document to the basket subcollection under the user's document
+            DocumentReference addedBundleRef = userRef.collection("basket").add(bundleData).get();
+            // Wait for the result
+            Map<String, Object> updatedBundleData = new HashMap<>();
+            updatedBundleData.put("cartBundleId", addedBundleRef.getId());
+            addedBundleRef.update(updatedBundleData);
+            // Return a response
+            return ResponseEntity.status(HttpStatus.CREATED).body("Bundle with ID: " + bundleId + " added to cart with ID: " + addedBundleRef.getId());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bundle with ID: " + bundleId + " does not exist");
+        }
+    }
+
     @GetMapping("/api/getBundles")
-    public String getBundles() {
+    public String getBundles() throws InterruptedException, ExecutionException {
         //required level: user
         var user = WebSecurityConfig.getUser();
 
-        /*
-         * Everything in here is absolute bs
-         * I am just using this as a temp to test a dynamic page
-         */
+        WebClient webClient = webClientBuilder.build();
+        String responseBody = webClient.get()
+                .uri("http://localhost:8080/suits")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+
+
+        // Return the list of product data for all bundles in the response
+        //return ResponseEntity.ok(allProductsData);
+//        String type="";
+//        String description="";
+        try {
+            // Convert JSON string to a Map or any other suitable data structure
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> data = objectMapper.readValue(responseBody, new TypeReference<Map<String, Object>>() {});
+
+            // Add the data to Firestore
+            db.collection("data").document("suits").set(data);
+
+            //return ResponseEntity.ok("Data copied to Firestore successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to copy data to Firestore");
+        }
+
+
+
+
+        try {
+            // Reference to the bundles collection in Firestore
+            CollectionReference bundlesRef = this.db.collection("bundle");
+
+            // Query to retrieve all documents in the bundles collection
+            Query query = bundlesRef;
+
+            // Execute the query and retrieve all bundle documents
+            QuerySnapshot querySnapshot = query.get().get();
+
+            // StringBuilder to construct the JSON string
+            StringBuilder jsonDataBuilder = new StringBuilder();
+            jsonDataBuilder.append("{\n");
+            jsonDataBuilder.append("  \"bundles\": [\n");
+
+
+            // Iterate over each document in the query result
+            for (QueryDocumentSnapshot document : querySnapshot) {
+                // Extract data from the document
+                String name = document.getString("name");
+                String description = document.getString("description");
+
+                // Append bundle details to the JSON string
+                jsonDataBuilder.append("    {\n");
+                jsonDataBuilder.append("      \"name\": \"").append(name).append("\",\n");
+                jsonDataBuilder.append("      \"description\": \"").append(description).append("\",\n");
+                jsonDataBuilder.append("      \"products\": [\n");
+
+                // Extract product references map from the document
+//                GenericTypeIndicator<Map<String, DocumentReference>> typeIndicator = new GenericTypeIndicator<Map<String, DocumentReference>>() {};
+//                Map<String, DocumentReference> productReferences = document.get("products", typeIndicator);
+                Map<String, Object> productReferences = (Map<String, Object>) document.get("products");
+
+
+                // Iterate over each product reference in the map
+                for (Map.Entry<String, Object> entry : productReferences.entrySet()) {
+                    // Retrieve product document reference
+                    DocumentReference productRef = (DocumentReference) entry.getValue();
+
+                    // Fetch product document from Firestore
+                    DocumentSnapshot productSnapshot = productRef.get().get();
+
+                    // Extract product data from the product document
+                    String productType = productSnapshot.getString("name");
+                    String productDescription = productSnapshot.getString("description");
+
+                    // Append product details to the JSON string
+                    jsonDataBuilder.append("        {\n");
+                    jsonDataBuilder.append("          \"name\": \"").append(productType).append("\",\n");
+                    jsonDataBuilder.append("          \"description\": \"").append(productDescription).append("\"\n");
+                    jsonDataBuilder.append("        },\n");
+                }
+
+                // Remove the trailing comma from the last product object
+                if (!productReferences.isEmpty()) {
+                    jsonDataBuilder.deleteCharAt(jsonDataBuilder.length() - 2); // Removes the last comma
+                }
+
+                // Append closing brackets for products array and bundle object
+                jsonDataBuilder.append("      ]\n");
+                jsonDataBuilder.append("    },\n");
+            }
+
+            // Remove the trailing comma from the last bundle object
+            if (!querySnapshot.isEmpty()) {
+                jsonDataBuilder.deleteCharAt(jsonDataBuilder.length() - 2); // Removes the last comma
+            }
+
+            // Append closing brackets for bundles array and JSON object
+            jsonDataBuilder.append("  ]\n");
+            jsonDataBuilder.append("}");
+
+            // Return the JSON string in the response
+            //return ResponseEntity.ok(jsonDataBuilder.toString());
+            return jsonDataBuilder.toString();
+        } catch (InterruptedException | ExecutionException e) {
+            // Handle exceptions appropriately
+            e.printStackTrace();
+            //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
 
 
         String jsonData = "{\n" +
@@ -55,8 +199,8 @@ class DBController {
                 "      \"description\": \"Bundle 1 description goes here.\",\n" +
                 "      \"products\": [\n" +
                 "        {\n" +
-                "          \"name\": \"Product 1\",\n" +
-                "          \"description\": \"Short description for Product 1. Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes\",\n" +
+                "          \"name\":  \"Product 1\",\n" +
+                "          \"description\": \"Short description for Product 1.\",\n" +
                 "          \"image\": \"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJqSUCfOuELtH0u5rBpf1Lnzy1Xp0lZgsblRa-mEM8_Q&s\"\n" +
                 "        },\n" +
                 "        {\n" +
@@ -96,36 +240,6 @@ class DBController {
                 "}";
 
         return jsonData;
-    }
-
-    @PostMapping("/api/addToCart")
-    public ResponseEntity<String> addToCart(@RequestBody String bundleId) throws ExecutionException, InterruptedException {
-        // Get the current user's ID
-        var user = WebSecurityConfig.getUser();
-
-        // Reference to the user's document
-        DocumentReference userRef = db.collection("user").document(user.getEmail());
-
-        // Reference to the bundle document
-        DocumentReference bundleRef = db.collection("bundles").document(bundleId);
-
-        // Get the bundle data
-        ApiFuture<DocumentSnapshot> bundleFuture = bundleRef.get();
-        DocumentSnapshot bundleSnapshot = bundleFuture.get();
-        if (bundleSnapshot.exists()) {
-            Map<String, Object> bundleData = bundleSnapshot.getData();
-
-            // Add the bundle document to the basket subcollection under the user's document
-            DocumentReference addedBundleRef = userRef.collection("basket").add(bundleData).get();
-            // Wait for the result
-            Map<String, Object> updatedBundleData = new HashMap<>();
-            updatedBundleData.put("cartBundleId", addedBundleRef.getId());
-            addedBundleRef.update(updatedBundleData);
-            // Return a response
-            return ResponseEntity.status(HttpStatus.CREATED).body("Bundle with ID: " + bundleId + " added to cart with ID: " + addedBundleRef.getId());
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bundle with ID: " + bundleId + " does not exist");
-        }
     }
 
     @GetMapping("/api/getCart")
@@ -175,6 +289,14 @@ class DBController {
     public String getProducts() {
         // Required level: user
         var user = WebSecurityConfig.getUser();
+
+        WebClient webClient = webClientBuilder.build();
+        String responseBody = webClient.get()
+                .uri("http://localhost:8080/suits")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
 
         // Dummy data
         String json = "{\n" +
