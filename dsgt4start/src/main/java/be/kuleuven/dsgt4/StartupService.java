efd7b7f702f73557a319c3c951711e06ccb7c511
bmas <benedictamarietta.amanda@student.kuleuven.be>
2024-06-11 16:16:44 +0200

    StartupService init

---
+++ dsgt4start/src/main/java/be/kuleuven/dsgt4/StartupService.java
@@ -0,0 +1,11 @@
+package be.kuleuven.dsgt4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class StartupService {
+
+}
